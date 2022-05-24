0d608a791ef28dfdf1a64328021873e0752eff17
FAISAL QURESHI <qureshifi@icloud.com>
2022-05-24 19:44:05 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navbar";
+import logo from "./logo.svg";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+function App() {
+  return <Navbar />;
+}
+
+export default App;
